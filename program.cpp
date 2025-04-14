698c1a4dca6c465d1bfa66f3995f166ad4fed62f
Nickolaus-899 <kolya.petukhov.03@mail.ru>
2025-04-14 16:14:57 +0300

    examples for c++20

--- program.cpp
+++ program.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <functional>
+#include <concepts>
 
 
 // Before C++ 14 (e.g. C++11) - no support of the feature
@@ -28,6 +29,11 @@ auto f_c20 = []<typename T>(T x) {
     return true;
 };
 
+template<typename T>
+concept Printable = requires(std::ostream& os, T val) {
+    { os << val } -> std::convertible_to<std::ostream&>;
+};
+
 
 int main() {
     f_c11(5);
@@ -52,6 +58,11 @@ int main() {
     int received_int = read_value.operator()<int>();
     std::cout << "Received result (C++ 20): " << received_int << "\n";
     free(buffer);
+
+    auto print_value = []<Printable T>(const T& val) {
+        std::cout << "Received param (C++ 20): " << val << "\n";
+    };
+    print_value("Hello!");
 
     return 0;
 }
